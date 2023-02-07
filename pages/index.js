79543a81760e11252ef1686ab0ebf80cca33e7da
ayushkumar <ayushverma8900@gmail.com>
2023-02-07 23:25:10 +0530

    second change

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,6 @@ export default function Home() {
     <>
       hello world
       <Image src={pic} alt="HELLO" height={500} width={500} priority />
-      hello uncle i think it is same as the react the only difference in this
-      version of the next js is prerendering of the html before instead of
-      client side rendering to reduce the initial loading time
       <Link href="/about">about</Link>
     </>
   );
